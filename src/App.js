744ddc98b63abab105cbc0573446d3439ba66bcb
argaputra12 <argaputra@student.uns.ac.id>
2022-11-08 11:39:39 +0700

    setup theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 function App() {
+  const [theme, colorMode] = useMode();
+
   return (
-    <div className="app flex relative">
-      
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app flex relative">
+          <main className="content">
+    
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
