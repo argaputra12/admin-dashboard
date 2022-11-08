5d972288092999371e45e750cc7a7cc567260a24
argaputra12 <argaputra@student.uns.ac.id>
2022-11-08 13:07:52 +0700

    delete tailwind, create topbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar  from "./scenes/global/Topbar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -8,9 +9,9 @@ function App() {
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <div className="app flex relative">
+        <div className="app">
           <main className="content">
-    
+            <Topbar />
           </main>
         </div>
       </ThemeProvider>
